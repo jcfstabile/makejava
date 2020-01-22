6ddbe1a41160f0be89f883c7f04ab7cd147c0769
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2020-01-22 01:12:58 -0300

    fix VPATH extraction

--- javamake.mk
+++ javamake.mk
@@ -23,7 +23,7 @@ test: build
 ### genera un makefile configurado automaticamente
 auto:
 	@echo "# makefile autoconfigurado on $(CURDIR)" > makefile
-	@echo VPATH = $(shell find -name '*.java' | sed 's%^.*/\(.*\)/.*$$%\1%g' | uniq | paste -sd':' ) >> makefile
+	@echo VPATH = $(shell find -name '*.java' | sed 's%^\./\(.*\)/.*$$%\1%g' | uniq | paste -sd':' ) >> makefile
 	@echo main = $(shell grep -r 'main.*String' */*.java | sed 's#^.*/\(.*java\):.*$$#\1#g' ) >> makefile
 	@echo sources = \$$\(main\) $(shell find -name '*.java' | sed 's#^.*/\(.*\)$$#\1#g' | paste -sd' ') >> makefile
 	@echo testclass = $(shell grep -H -r "import[[:space:]]*static[[:space:]]*org.junit" tests/*.java | sed 's#^tests/\(.*\)\.java:.*$$#\1#g') >> makefile
