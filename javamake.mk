f7fb5e427146174fb8e08bec589b24455ab23b6c
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2020-01-22 18:46:23 -0300

    fix VPATH to bin on $(classpath), add bin as dest

--- javamake.mk
+++ javamake.mk
@@ -2,17 +2,17 @@
 #
 # template start
 .PHONY: clean build run test
-# Donde esten los prerequisitos iran a parar las clases... por eso VPATH
-classpath = .:$(VPATH):/usr/share/java/junit4.jar
+# bin existe
+classpath = .:bin:/usr/share/java/junit4.jar
 
 build: $(sources:.java=.class)
 	@echo build done
 
 %.class : %.java
-	javac -cp $(classpath) $<
+	javac -d bin -cp $(classpath) $<
 
 clean:
-	find -name '*.class' -exec rm -v {} \+
+	find bin -name '*.class' -exec rm -v {} \+
 
 run: $(main:.java=.class)
 	@java -cp $(classpath) $(main:.java=)
@@ -24,7 +24,7 @@ test: build
 auto:
 	@echo "# makefile autoconfigurado on $(CURDIR)" > makefile
 	@echo VPATH = $(shell find -name '*.java' | sed 's%^\./\(.*\)/.*$$%\1%g' | uniq | paste -sd':' ) >> makefile
-	@echo main = $(shell grep -r 'main.*String' */*.java | sed 's#^.*/\(.*java\):.*$$#\1#g' ) >> makefile
+	@echo main = $(shell grep -H -r 'main.*String' */*.java | sed 's#^.*/\(.*java\):.*$$#\1#g' ) >> makefile
 	@echo sources = \$$\(main\) $(shell find -name '*.java' | sed 's#^.*/\(.*\)$$#\1#g' | paste -sd' ') >> makefile
 	@echo testclass = $(shell grep -H -r "import[[:space:]]*static[[:space:]]*org.junit" tests/*.java | sed 's#^tests/\(.*\)\.java:.*$$#\1#g') >> makefile
 	@cat javamake.mk >> makefile
