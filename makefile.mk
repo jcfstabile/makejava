364577b5f53c4294c3556715cbe7e5db9125076f
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2020-01-22 00:29:55 -0300

    keep original makefile as .mk

---
+++ makefile.mk
@@ -0,0 +1,40 @@
+#.SUFFIXES: .java .class
+.PHONY: clean build run test
+
+#donde buscar los prerequisitos (separados por blanks o colons)
+# subdirs que coincidaran con los nombres de los *package*
+VPATH = bin:vehicle:tests
+
+
+#java file (con el .java) donde este el main ,si no hay, dejar: main = 
+main = Vehicle.java
+
+#java files (con el .java)
+sources = $(main) 
+sources += Bicycle.java
+sources += Car.java
+sources += Tests.java
+
+#Test class (sin .class)
+testclass = Tests
+
+#This is a list of dirs or jar files used to compile the source files.
+#donde esten los prereq iran a parar las clases...
+#por eso el VPATH
+classpath = .:$(VPATH):/usr/share/java/junit4.jar
+
+build: $(sources:.java=.class)
+	@echo Build done
+
+%.class : %.java
+	javac -d bin -cp $(classpath) $<
+
+# https://unix.stackexchange.com/a/116390/103956
+clean:
+	find . -type f -name '*.class' -exec rm -v {} +
+
+run: $(main:.java=.class)
+	@java -cp $(classpath) $(main:.java=)
+
+test: build
+	@java -cp $(classpath) org.junit.runner.JUnitCore $(testclass)
