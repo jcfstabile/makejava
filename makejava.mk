e662ed551b9c88959f2b97f6e640d328de184216
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2020-02-01 19:27:15 -0300

    move vars defs to makejava body, add gitignore

--- makejava.mk
+++ makejava.mk
@@ -2,13 +2,20 @@
 #
 # template start
 .PHONY: build clean run test
-	# https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.6.0/junit-platform-console-standalone-1.6.0.jar
+# https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.6.0/junit-platform-console-standalone-1.6.0.jar
+VPATH = $(shell find -name '*.java' | sed 's%^\./\(.*\)/.*$$%\1%g' | uniq | paste -sd':' )
+main = $(shell grep -H -r 'main.*String' */*.java | sed 's%^.*/\(.*java\):.*$$%\1%g' )
+sources = $(main) $(shell find -name '*.java' | sed 's%^.*/\(.*\)$$%\1%g' | paste -sd' ')
+testclass = $(shell grep -H -r "import[[:space:]]*org.junit.Test;" tests/*.java | sed 's%^tests/\(.*\)\.java:.*$$%\1%g')
 # bin existe
 classpath = .:bin:src:lib/*
 vpath %.class $(subst src,bin,$(VPATH)):bin
 current := $(main) $(shell find src tests  -name '*java' |sed 's%^.*/\(.*\)$$%\1%g' | paste -sd' ')
 
-build: $(sources:.java=.class) lib/junit-platform-console-standalone-1.6.0.jar
+#prueba :
+#	@echo VPATH $(VPATH)
+
+build: lib/junit-platform-console-standalone-1.6.0.jar $(sources:.java=.class) 
 ifeq ($(sources), $(current))
 	@echo Updated
 else
@@ -33,11 +40,7 @@ test: build
 #	@java -cp $(classpath) org.junit.runner.JUnitCore $(testclass) | sed '/^[[:space:]].*/d' 
 	java -jar lib/* -cp $(classpath) --scan-class-path | sed '/^[[ ] .*/d'
 ### genera un makefile configurado automaticamente
-auto:
-	@echo "# makefile autoconfigurado on $(CURDIR)" > makefile
-	@echo VPATH = $(shell find -name '*.java' | sed 's%^\./\(.*\)/.*$$%\1%g' | uniq | paste -sd':' ) >> makefile
-	@echo main = $(shell grep -H -r 'main.*String' */*.java | sed 's#^.*/\(.*java\):.*$$#\1#g' ) >> makefile
-	@echo sources = \$$\(main\) $(shell find -name '*.java' | sed 's#^.*/\(.*\)$$#\1#g' | paste -sd' ') >> makefile
-	@echo testclass = $(shell grep -H -r "import[[:space:]]*org.junit.Test;" tests/*.java | sed 's#^tests/\(.*\)\.java:.*$$#\1#g') >> makefile
-	@cat makejava.mk >> makefile
-	@make
+#auto:
+#	@echo "# makefile autoconfigurado on $(CURDIR)" > makefile
+#	@cat makejava.mk >> makefile
+#	@make
