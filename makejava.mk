de8934e05e4ca43faef3efd76cef698e50e87129
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2020-03-10 01:59:39 -0300

    add usage (as default) and describe rules

--- makejava.mk
+++ makejava.mk
@@ -12,6 +12,28 @@ testclass = $(shell find -name '*.java' -exec grep "import *org.junit" {} \+ | s
 classpath = .:bin:src:lib/*
 vpath %.class $(subst src,bin,$(VPATH)):bin
 e = \033
+greenbg = "\$e[42m"
+reset   = "\$e[0m"
+
+usage : 
+	@echo Type "${greenbg}" make describe "${reset}" for info or
+	@echo "${greenbg}" make -f /path/to/makejava/makejava.mk configure "${reset}"
+	@echo to install makefile in current directory and start to build java project
+
+describe : 
+	@echo "                $e[4m" makejava.mk "$e[0m"
+	@echo
+	@echo makejava.mk is a makefile to handle the build of a java project.
+	@echo After cloned from github or copied by others means, from the working 
+	@echo directory of the java project, execute 
+	@echo -n "$e[7m"
+	@echo make -f /path/to/makejava/makejava.mk configure
+	@echo -n "$e[0m"
+	@echo The above command will create a makefile in the working directory.
+	@echo Now use this makefile to handle the project. i.e.
+	@echo
+	@echo "make build ; make run ; make test"
+	@echo 
 
 build: bin/ lib/junit-platform-console-standalone-1.6.0.jar $(sources:.java=.class) 
 	@echo "$e[92mBuild done.$e[0m"
@@ -43,7 +65,7 @@ test: build
 	@echo "$e[92mRunning tests:$e[0m" 
 	java -jar lib/* -cp $(classpath) --scan-class-path | sed '/^[[ ] .*/d'
 
-check :
+checkvars :
 	@echo "$e[91mVPATH$e[0m" $(VPATH)
 	@echo "$e[91mmainfqn$e[0m" $(mainfqn)
 	@echo "$e[91mmainclass$e[0m" $(mainclass)
@@ -57,8 +79,8 @@ configure :
 	@echo "# makefile autoconfigurado on $(CURDIR)" > makefile
 	# $(dir $(MAKEFILE_LIST)) is absolute path
 	@cat $(MAKEFILE_LIST) >> makefile
-	# makefile written on $(shell pwd)
-	# use: make ; make run ; make test
+	@echo "$e[40m" makefile written on $(shell pwd) "$e[0m"
+	@echo "# use: make build ; make run ; make test"
 
 tips : tipsbanner imports
 
@@ -66,7 +88,7 @@ tipsbanner :
 	@echo -n "$e[94m"
 	@echo Basics imports needed to include in test classes
 	@echo "ie. insert easily with: $e[95m make imports >> src/ClassTests.java"
-	@echo -n "$e[0m"
+	@echo "$e[0m"
 
 imports :
 	@echo import static org.junit.jupiter.api.Assertions.assertArrayEquals\;
