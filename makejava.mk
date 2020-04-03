535bdd53dd1093bbf178629f013503f1d85a5118
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2020-04-03 17:52:08 -0300

    Add create link to junit instead of download if not in makejava root dir

--- makejava.mk
+++ makejava.mk
@@ -12,87 +12,88 @@ testclass = $(shell find -name '*.java' -exec grep "import *org.junit" {} \+ | s
 classpath = .:bin:src:lib/*
 vpath %.class $(subst src,bin,$(VPATH)):bin
 e = \033
-greenbg = "\$e[42m"
-reset   = "\$e[0m"
+greenbg = "\${e}[42m"
+redfg = "\${e}[91m"
+reset   = "\${e}[0m"
+download = echo -e "${e}[92mDownloading junit 5 platform console:${e}[0m"; mkdir -p lib; wget -nv -P lib https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.6.0/junit-platform-console-standalone-1.6.0.jar;
+makelink = ln -s $(dir $(MAKEFILE_LIST))/lib lib;
 
 usage : 
-	@echo Type "${greenbg}" make describe "${reset}" for info or
-	@echo "${greenbg}" make -f /path/to/makejava/makejava.mk configure "${reset}"
-	@echo to install makefile in current directory and start to build java project
+	@echo -e Type "${greenbg}" make describe "${reset}" for info or
+	@echo -e "${greenbg}" make -f /path/to/makejava/makejava.mk configure "${reset}"
+	@echo -e to install makefile in current directory and start to build java project
 
 describe : 
-	@echo "                $e[4m" makejava.mk "$e[0m"
+	@echo -e "                ${e}[4m" makejava.mk "${e}[0m"
 	@echo
-	@echo makejava.mk is a makefile to handle the build of a java project.
-	@echo After cloned from github or copied by others means, from the working 
-	@echo directory of the java project, execute 
-	@echo -n "$e[7m"
-	@echo make -f /path/to/makejava/makejava.mk configure
-	@echo -n "$e[0m"
-	@echo The above command will create a makefile in the working directory.
-	@echo Now use this makefile to handle the project. i.e.
+	@echo -e makejava.mk is a makefile to handle the build of a java project.
+	@echo -e After cloned from github or copied by others means, from the working 
+	@echo -e directory of the java project, execute 
+	@echo -e -n "${e}[7m"
+	@echo -e make -f /path/to/makejava/makejava.mk configure
+	@echo -e -n "${e}[0m"
+	@echo -e The above command will create a makefile in the working directory.
+	@echo -e Now use this makefile to handle the project. i.e.
 	@echo
-	@echo "make build ; make run ; make test"
-	@echo 
+	@echo -e "make build ; make run ; make test"
+	@echo -e 
 
 build: bin/ lib/junit-platform-console-standalone-1.6.0.jar $(sources:.java=.class) 
-	@echo "$e[92mBuild done.$e[0m"
+	@echo -e "${e}[92mBuild done.${e}[0m"
 
 bin/ :
 	mkdir -p bin
 
 lib/junit-platform-console-standalone-1.6.0.jar :
-	@echo "$e[92mDownloading junit 5 platform console:$e[0m" 
-	@mkdir -p lib
-	@wget -nv -P lib https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.6.0/junit-platform-console-standalone-1.6.0.jar
+	@if [ -x "makejava.mk" ]; then $(download) else $(makelink) fi
 
 %.class: %.java
 	javac -d bin -cp $(classpath) $<
 
 clean:
-	@echo "$e[92mCleaning bin dir:$e[0m" 
+	@echo -e "${e}[92mCleaning bin dir:${e}[0m" 
 	find bin -name '*.class' -exec rm -v {} \+
 
 run: build $(mainclass:=.class)
 ifneq ($(strip $(mainclass)),)
-	@echo "$e[92mRunning main method found:$e[0m" 
+	@echo -e "${e}[92mRunning main method found:${e}[0m" 
 	@java -cp $(classpath) $(mainfqn)
 else
-	@echo "$e[93mNo main method found.$e[0m" 
+	@echo -e "${e}[93mNo main method found.${e}[0m" 
 endif
 
 test: build
-	@echo "$e[92mRunning tests:$e[0m" 
+	@echo -e "${e}[92mRunning tests:${e}[0m" 
 	java -jar lib/* -cp $(classpath) --scan-class-path | sed '/^[[ ] .*/d'
 
 checkvars :
-	@echo "$e[91mVPATH$e[0m" $(VPATH)
-	@echo "$e[91mmainfqn$e[0m" $(mainfqn)
-	@echo "$e[91mmainclass$e[0m" $(mainclass)
-	@echo "$e[91msources$e[0m" $(sources)
-	@echo "$e[91mtestclass$e[0m" $(testclass)
-	@echo "$e[91mclasspath$e[0m" $(classpath)
+	@echo -e "${e}[91mVPATH${e}[0m" $(VPATH)
+	@echo -e "${e}[91mmainfqn${e}[0m" $(mainfqn)
+	@echo -e "${e}[91mmainclass${e}[0m" $(mainclass)
+	@echo -e "${e}[91msources${e}[0m" $(sources)
+	@echo -e "${e}[91mtestclass${e}[0m" $(testclass)
+	@echo -e "${e}[91mclasspath${e}[0m" $(classpath)
 
 
 ### genera un makefile configurado automaticamente
 configure :
-	@echo "# makefile autoconfigurado on $(CURDIR)" > makefile
+	@echo -e "# makefile autoconfigurado on $(CURDIR)" > makefile
 	# $(dir $(MAKEFILE_LIST)) is absolute path
 	@cat $(MAKEFILE_LIST) >> makefile
-	@echo "$e[40m" makefile written on $(shell pwd) "$e[0m"
-	@echo "# use: make build ; make run ; make test"
+	@echo -e "${e}[40m" makefile written on $(shell pwd) "${e}[0m"
+	@echo -e "# use: make build ; make run ; make test"
 
 tips : tipsbanner imports
 
 tipsbanner : 
-	@echo -n "$e[94m"
-	@echo Basics imports needed to include in test classes
-	@echo "ie. insert easily with: $e[95m make imports >> src/ClassTests.java"
-	@echo "$e[0m"
+	@echo -e -n "${e}[94m"
+	@echo -e Basics imports needed to include in test classes
+	@echo -e "ie. insert easily with: ${e}[95m make imports >> src/ClassTests.java"
+	@echo -e "${e}[0m"
 
 imports :
-	@echo import static org.junit.jupiter.api.Assertions.assertArrayEquals\;
-	@echo import static org.junit.jupiter.api.Assertions.assertEquals\;
-	@echo import org.junit.jupiter.api.Test\;
-	@echo import org.junit.jupiter.api.BeforeEach\;
+	@echo -e import static org.junit.jupiter.api.Assertions.assertArrayEquals\;
+	@echo -e import static org.junit.jupiter.api.Assertions.assertEquals\;
+	@echo -e import org.junit.jupiter.api.Test\;
+	@echo -e import org.junit.jupiter.api.BeforeEach\;
 
